3a55d3f071916851875babb6c8fe21798c0f5951
DelonteKelly <kellylegacyestatesllc@gmail.com>
2026-06-11 17:56:49 -0400

    temp: Create temporary App.tsx for import resolution

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default App;
